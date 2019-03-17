94d35660fa0300b963c3277d0106d757a23d6513
ek20002 <emad.kavosi@yahoo.com>
2019-03-17 14:22:18 +0330

    added it_store_new_customer_into_database_and_return_access_token_to_front_end

--- tests/Feature/Authenticate/RegisterUserTest.php
+++ tests/Feature/Authenticate/RegisterUserTest.php
@@ -2,11 +2,13 @@
 
 namespace Tests\Feature\Authenticate;
 
+use App\Models\Token;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class RegisterUserTest extends TestCase {
+class RegisterUserTest extends TestCase
+{
     use RefreshDatabase;
 
     #-------------------------------------##   <editor-fold desc="setUp">   ##----------------------------------------------------#
@@ -55,12 +57,43 @@ class RegisterUserTest extends TestCase {
     }
 
 
-    # </editor-fold>
+    /** @test */
+
+    public function it_require_the_none_empty_name()
+    {
+        $this->setData(['name' => null])
+            ->register()
+            ->assertStatus(422)
+            ->assertJsonValidationErrors('name');
+
+    }
 
 
     /** @test */
-    public function it_store_new_customer_into_database_and_return_access_token_to_front_end()
+
+    public function it_require_the_none_empty_last_name()
     {
+        $this->setData(['last_name' => null])
+            ->register()
+            ->assertStatus(422)
+            ->assertJsonValidationErrors('last_name');
 
     }
+
+
+    # </editor-fold>
+
+
+        /** @test */
+    public function it_store_new_customer_into_database_and_return_access_token_to_front_end()
+    {
+        $user = $this->setData()->data;
+        $this->postJson(
+            route('register'), $user
+        )->assertJsonStructure(['data']);
+        $this->assertDatabaseHas('users', $user);
+        $this->assertCount(1,Token::all());
+    }
+
+
 }
