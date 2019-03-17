75886ca9aaedfbe96cc534d008b0c5174d0b976c
hasanteymoori <hasan.teymoori1372@gmail.com>
2019-03-17 12:41:21 +0330

    register new customer feature testing (prepare for Emad)

---
+++ tests/Feature/Authenticate/RegisterUserTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests\Feature\Authenticate;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class RegisterUserTest extends TestCase {
+    use RefreshDatabase;
+
+    #-------------------------------------##   <editor-fold desc="setUp">   ##----------------------------------------------------#
+
+    /**
+     * @var $data
+     */
+    protected $data;
+
+    /**
+     * set data property
+     *
+     * @param array $override
+     * @return RegisterUserTest
+     */
+    protected function setData($override = [])
+    {
+        $this->data = raw(User::class, $override);
+
+        return $this;
+    }
+
+    /**
+     * send the request to register the user
+     *
+     * @return \Illuminate\Foundation\Testing\TestResponse
+     */
+    protected function register()
+    {
+        return $this->postJson(
+            route('register'), $this->data
+        );
+    }
+
+    # </editor-fold>
+
+    #-------------------------------------##   <editor-fold desc="The Validation">   ##----------------------------------------------------#
+
+    /** @test */
+    public function it_required_the_valid_mobile()
+    {
+        $this->setData(['mobile' => null])
+            ->register()
+            ->assertStatus(422)
+            ->assertJsonValidationErrors('mobile');
+    }
+
+
+    # </editor-fold>
+
+}
