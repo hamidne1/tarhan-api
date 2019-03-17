eb69091e743afb7e540b4b0091f210a1d8cd3833
hasanteymoori <hasan.teymoori1372@gmail.com>
2019-03-17 14:36:21 +0330

    update register user test

--- tests/Feature/Authenticate/RegisterUserTest.php
+++ tests/Feature/Authenticate/RegisterUserTest.php
@@ -7,8 +7,7 @@ use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-class RegisterUserTest extends TestCase
-{
+class RegisterUserTest extends TestCase {
     use RefreshDatabase;
 
     #-------------------------------------##   <editor-fold desc="setUp">   ##----------------------------------------------------#
@@ -48,51 +47,42 @@ class RegisterUserTest extends TestCase
     #-------------------------------------##   <editor-fold desc="The Validation">   ##----------------------------------------------------#
 
     /** @test */
-    public function it_required_the_valid_mobile()
+    public function it_required_the_valid_mobile_for_user()
     {
         $this->setData(['mobile' => null])
             ->register()
             ->assertStatus(422)
             ->assertJsonValidationErrors('mobile');
+
+        $this->setData(['mobile' => '0913000043'])
+            ->register()
+            ->assertStatus(422)
+            ->assertJsonValidationErrors('mobile');
     }
 
-
     /** @test */
-
-    public function it_require_the_none_empty_name()
+    public function it_required_the_valid_name_for_user()
     {
         $this->setData(['name' => null])
             ->register()
             ->assertStatus(422)
             ->assertJsonValidationErrors('name');
-
     }
-
-
-    /** @test */
-
-    public function it_require_the_none_empty_last_name()
-    {
-        $this->setData(['last_name' => null])
-            ->register()
-            ->assertStatus(422)
-            ->assertJsonValidationErrors('last_name');
-
-    }
-
 
     # </editor-fold>
 
-
-        /** @test */
-    public function it_store_new_customer_into_database_and_return_access_token_to_front_end()
+    /** @test */
+    public function it_store_new_customer_into_database_and_return_access_token_to_the_front_end()
     {
-        $user = $this->setData()->data;
-        $this->postJson(
-            route('register'), $user
-        )->assertJsonStructure(['data']);
-        $this->assertDatabaseHas('users', $user);
-        $this->assertCount(1,Token::all());
+        $this->setData()
+            ->register()
+            ->assertStatus(201)
+            ->assertJsonStructure([
+                'data'
+            ]);
+
+        $this->assertDatabaseHas('users', $this->data);
+        $this->assertCount(1, Token::all());
     }
 
 
