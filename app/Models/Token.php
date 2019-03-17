3bd9f9c7a6ebf6075233f5d084ad98d48b5293e4
hasanteymoori <hasan.teymoori1372@gmail.com>
2019-03-17 09:55:39 +0330

    update token model and add BaseModel

--- app/Models/Token.php
+++ app/Models/Token.php
@@ -2,9 +2,33 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+/**
+ * Class Token
+ *
+ * @method static |Token create($data)
+ * @method static|\Illuminate\Database\Eloquent\Builder where($column, $operator = null, $value = null, $boolean = 'and')
+ */
+class Token extends Model {
 
-class Token extends Model
-{
-    //
+    /**
+     * {@inheritDoc}
+     */
+    protected $guarded = [
+        'id', 'user_id'
+    ];
+
+    #-------------------------------------##   <editor-fold desc="The RelationShips">   ##----------------------------------------------------#
+
+    /**
+     * token user
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function User()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    # </editor-fold>
 }
+
