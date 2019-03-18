5a6bf59fee9cb44bbbc92a78c65577e615fcbc4c
hasanteymoori <hasan.teymoori1372@gmail.com>
2019-03-18 16:51:07 +0330

    add tariff controller starter

---
+++ app/Http/Controllers/TariffsController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\TariffResource;
+use App\Models\Tariff;
+use Illuminate\Http\Request;
+
+class TariffsController extends Controller {
+
+    /**
+     * TariffsController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:admin')->except('index');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
+     */
+    public function index()
+    {
+        return TariffResource::collection(
+            Tariff::all()
+        );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'title' => 'required',
+            'sub_title' => 'required'
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
