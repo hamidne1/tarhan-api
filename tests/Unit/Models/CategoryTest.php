c2d3bc1a1a2b5a0dbb9ead7741fe37c3687620e2
hasanteymoori <hasan.teymoori1372@gmail.com>
2019-03-18 11:39:26 +0330

    update category test

--- tests/Unit/Models/CategoryTest.php
+++ tests/Unit/Models/CategoryTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit\Models;
 
+use App\Models\Catalog;
 use App\Models\Category;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -20,6 +21,11 @@ class CategoryTest extends TestCase {
     protected $category;
 
     /**
+     * @var Catalog $catalog
+     */
+    protected $catalog;
+
+    /**
      * Setup the test environment.
      *
      * @return void
@@ -27,7 +33,10 @@ class CategoryTest extends TestCase {
     protected function setUp(): void
     {
         parent::setUp();
-        $this->category = create(Category::class);
+        $this->catalog = create(Catalog::class);
+        $this->category = create(Category::class, [
+            'catalog_id' => $this->catalog->id
+        ]);
     }
 
     /** @test */
@@ -69,33 +78,14 @@ class CategoryTest extends TestCase {
         $this->assertGuard(['slug' => 'The fake slug value']);
     }
 
-    /** @test */
-    public function it_should_guarded_the_level_field()
-    {
-        $this->assertGuard(['level' => 999]);
-    }
-
-    /** @test */
-    public function it_should_guarded_the_parent_id_field()
-    {
-        $this->assertGuard(['parent_id' => 999]);
-    }
-
     # </editor-fold>
 
     #-------------------------------------##   <editor-fold desc="The Booting">   ##----------------------------------------------------#
-
 
     /** @test */
     public function it_attach_slug_when_creating_attribute_group()
     {
         $this->assertArrayHasKey('slug', $this->category->toArray());
-    }
-
-    /** @test */
-    public function it_attach_level_when_creating_attribute_group()
-    {
-        $this->assertArrayHasKey('level', $this->category->toArray());
     }
 
     /** @test */
@@ -108,59 +98,16 @@ class CategoryTest extends TestCase {
         $this->assertNotEquals($this->category->slug, Str::slug($label));
     }
 
-    /** @test
-     */
-    public function after_delete_a_category_all_children_also_deleted_at_the_same_time()
-    {
-        $child = create(Category::class, ['parent_id' => $this->category->id]);
-        $childOfTheChild = create(Category::class, ['parent_id' => $child->id]);
-
-        try {
-            $this->category->delete();
-        } catch (\Exception $e) {
-        }
-
-        $this->assertDatabaseMissing('categories', $this->category->toArray());
-        $this->assertDatabaseMissing('categories', $child->toArray());
-        $this->assertDatabaseMissing('categories', $childOfTheChild->toArray());
-    }
-
     # </editor-fold>
 
     #-------------------------------------##   <editor-fold desc="The RelationShips">   ##----------------------------------------------------#
 
     /** @test */
-    public function it_belongs_to_a_parent()
+    public function it_belongs_to_a_catalog()
     {
-        $child = create(Category::class, ['parent_id' => $this->category->id]);
-
-        $this->assertEquals($child->parent->toArray(), $this->category->toArray());
-    }
-
-    /** @test */
-    public function it_has_many_children()
-    {
-        $children = create(Category::class, [
-            'parent_id' => $this->category->id
-        ], 2);
-
-        $children->each(function ($child) {
-            $this->assertTrue($this->category->children->contains($child));
-        });
-    }
-
-    # </editor-fold>
-
-    #-------------------------------------##   <editor-fold desc="The Methods">   ##----------------------------------------------------#
-
-    /** @test */
-    public function it_can_add_new_category()
-    {
-        $this->category->addCategory(
-            raw(Category::class), $this->category->id
+        $this->assertEquals(
+            $this->category->id, $this->catalog->category->id
         );
-
-        $this->assertCount(1, $this->category->children);
     }
 
     # </editor-fold>
@@ -173,7 +120,7 @@ class CategoryTest extends TestCase {
         $category = create('App\Models\Category');
 
         $this->assertEquals(
-            $category->slug, str_slug($category->label)
+            $category->slug, Str::slug($category->lable)
         );
     }
 
