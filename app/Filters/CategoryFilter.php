2faf102fed6f5dca7afe2471e2db295d6e40e7e2
hasanteymoori <hasan.teymoori1372@gmail.com>
2019-03-18 12:25:20 +0330

    catalog resource and controller

--- app/Filters/CategoryFilter.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-namespace App\Filters;
-
-
-class CategoryFilter extends AbstractFilters {
-
-}
--- app/Http/Controllers/CatalogsController.php
+++ app/Http/Controllers/CatalogsController.php
@@ -2,62 +2,73 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\CatalogResource;
+use App\Models\Catalog;
 use Illuminate\Http\Request;
 
-class CatalogsController extends Controller
-{
+class CatalogsController extends Controller {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
      */
     public function index()
     {
-        //
+        return CatalogResource::collection(
+            Catalog::all()
+        );
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Validation\ValidationException
      */
     public function store(Request $request)
     {
-        //
+        $validated = $this->validate($request, [
+            'title' => 'required|unique:catalogs',
+            'label' => 'required|unique:catalogs'
+        ]);
+
+        $catalog = Catalog::create($validated);
+
+        return $this->respond(
+            'یک کاتالوگ جدید ایجاد شد', new CatalogResource($catalog)
+        );
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Validation\ValidationException
      */
     public function update(Request $request, $id)
     {
-        //
+        $validated = $this->validate($request, [
+            'title' => 'required|unique:catalogs',
+            'label' => 'required|unique:catalogs'
+        ]);
+
+        Catalog::findOrFail($id)->update($validated);
+
+        return $this->respond('بروزرسانی با موفقیت انجام شد');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
-        //
+        return Catalog::findOrFail($id)->delete();
     }
 }
