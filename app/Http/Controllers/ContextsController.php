ea179f61156220ed8e8b77cc655d9f8726fac619
hasanteymoori <hasan.teymoori1372@gmail.com>
2019-03-30 13:20:08 +0430

    context, page, widget controller

---
+++ app/Http/Controllers/ContextsController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\ContextResource;
+use App\Models\Context;
+use Illuminate\Http\Request;
+
+class ContextsController extends Controller {
+    /**
+     *
+     * ContextsController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:admin');
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function store(Request $request)
+    {
+        $validated = $this->validate($request, [
+            'page_id' => 'required|exists:pages,id',
+            'parent_id' => 'nullable|exists:contexts,id',
+            'slug' => 'required|unique:contexts,slug',
+            'icon' => 'nullable',
+            'href' => 'nullable|url',
+            'value' => 'required',
+        ]);
+
+        $context = \App\Models\Page::findOrFail($validated['page_id'])->addContext($validated);
+
+        return $this->respondCreated(
+            'یک محتوای متنی جدید ایجاد شد', new ContextResource($context)
+        );
+    }
+
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function update(Request $request, $id)
+    {
+        $validated = $this->validate($request, [
+            'page_id' => 'required|exists:pages,id',
+            'parent_id' => 'nullable|exists:contexts,id',
+            'slug' => 'required|unique:contexts,slug',
+            'icon' => 'nullable',
+            'href' => 'nullable|url',
+            'value' => 'required',
+        ]);
+
+        Context::findOrFail($id)->update($validated);
+
+        return $this->respond(
+            'کانتکست مورد نظر بروزرسانی شد'
+        );
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Exception
+     */
+    public function destroy($id)
+    {
+        Context::findOrFail($id)->delete();
+
+        return $this->respondDeleted();
+    }
+}
