5b457c75a43ad0cef5d683b4d87afae67021a3b5
hasanteymoori <hasan.teymoori1372@gmail.com>
2019-03-30 13:18:41 +0430

    context, page, widget migration

---
+++ database/migrations/2019_03_30_143549_create_widgets_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWidgetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('widgets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('category_id')->nullable();
+            $table->unsignedInteger('page_id')->nullable();
+            $table->string('col');
+            $table->enum('group', \App\Enums\ContentGroupEnum::values());
+            $table->string('title');
+            $table->string('alt');
+            $table->string('href');
+            $table->string('src');
+
+            $table->foreign('category_id')
+                ->references('id')->on('categories')
+                ->onDelete('set null')->onUpdate('cascade');
+
+            $table->foreign('page_id')
+                ->references('id')->on('pages')
+                ->onDelete('set null')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('widgets');
+    }
+}
