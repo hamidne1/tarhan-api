81b1c73c52169181a1c3c16f67960c8406bded04
hasanteymoori <hasan.teymoori1372@gmail.com>
2019-03-31 12:33:18 +0430

    create two enum for transaction

--- database/migrations/2019_03_31_073024_create_transactions_table.php
+++ database/migrations/2019_03_31_073024_create_transactions_table.php
@@ -15,7 +15,25 @@ class CreateTransactionsTable extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->enum('port', \App\Enums\Gateway\TransactionPortEnum::values());
+            $table->unsignedInteger('receipt_id')->nullable();
+            $table->decimal('price');
+            $table->string('ref_id')->nullable();
+            $table->string('tracking_code')->nullable();
+            $table->string('ip', 20)->nullable();
+            $table->string('card_number', 50)->nullable();
+            $table->enum('status', [
+                \App\Enums\Gateway\TransactionStatusEnum::values()
+            ])->default(\App\Enums\Gateway\TransactionStatusEnum::Init);
+
+            $table->integer('result_code')->nullable();
+            $table->string('result_message')->nullable();
             $table->timestamps();
+            $table->timestamp('paid_at')->nullable();
+
+            $table->foreign('receipt')
+                ->references('id')->on('Receipts')
+                ->onDelete('set null')->onUpdate('cascade');
         });
     }
 
