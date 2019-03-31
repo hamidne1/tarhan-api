85c43e172dc35a6ce9eb5a61faa06f66b1ea6752
hasanteymoori <hasan.teymoori1372@gmail.com>
2019-03-31 15:14:09 +0430

    fix factory bug of transaction

--- database/factories/TransactionFactory.php
+++ database/factories/TransactionFactory.php
@@ -4,8 +4,8 @@ use Faker\Generator as Faker;
 
 $factory->define(\App\Models\Transaction::class, function (Faker $faker) {
     return [
-        'order_id' => function(){
-            return factory(\App\Models\Order::class)->create()->id;
+        'receipt_id' => function(){
+            return factory(\App\Models\Receipt::class)->create()->id;
         },
         'port' => $faker->randomElement(\App\Enums\GateWay\TransactionPortEnum::values()),
         'price' => 10000,
