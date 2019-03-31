a1d07c06cd94aae079b695a213c2f0d22c586e40
hasanteymoori <hasan.teymoori1372@gmail.com>
2019-03-31 11:59:09 +0430

    migration of orders

---
+++ database/migrations/2019_03_30_120327_create_orders_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->unsignedInteger('user_id')->nullable();
+            $table->unsignedBigInteger('price');
+
+            $table->text('title');
+            $table->text('description')->nullable();
+
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('set null')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
