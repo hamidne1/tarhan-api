a9773f795193d7d30b8d4562c29c3be8a5e7ddb8
hasanteymoori <hasan.teymoori1372@gmail.com>
2019-03-31 12:53:49 +0430

    create receipt testing and modeling

--- app/Models/Receipt.php
+++ app/Models/Receipt.php
@@ -2,9 +2,41 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+/**
+ * @property integer id
+ *
+ * @property \Illuminate\Support\Collection transactions
+ */
+class Receipt extends Model {
+    /**
+     * {@inheritDoc}
+     */
+    protected $guarded = [
+        'id', 'order_id'
+    ];
 
-class Receipt extends Model
-{
-    //
+
+    #-------------------------------------##   <editor-fold desc="The RelationShips">   ##----------------------------------------------------#
+
+    /**
+     * receipt order
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    /**
+     * receipt transactions
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function transactions()
+    {
+        return $this->hasMany(Transaction::class);
+    }
+
+    # </editor-fold>
 }
---
+++ tests/Unit/Models/ReceiptTest.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use App\Models\Receipt;
+use App\Models\Transaction;
+use Tests\TestCase;
+
+class ReceiptTest extends TestCase {
+    #-------------------------------------##   <editor-fold desc="setUp">   ##----------------------------------------------------#
+
+    /**
+     * @var Receipt $receipt
+     */
+    protected $receipt;
+
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->receipt = create(Receipt::class);
+    }
+
+    /** @test */
+    public function it_should_extends_base_model()
+    {
+        $this->assertTrue(
+            is_subclass_of(
+                $this->receipt, 'App\Models\Model'
+            )
+        );
+    }
+
+    # </editor-fold>
+
+    #-------------------------------------##   <editor-fold desc="The Guarded">   ##----------------------------------------------------#
+
+    /**
+     * checking guard data
+     *
+     * @param array $guardData
+     */
+    protected function assertGuard(array $guardData)
+    {
+        $this->receipt->update(
+            raw(Receipt::class, $guardData)
+        );
+        $this->assertDatabaseMissing('receipts', $guardData);
+    }
+
+    /** @test */
+    public function it_should_guarded_the_id_field()
+    {
+        $this->assertGuard(['id' => 14048343]);
+    }
+
+    /** @test */
+    public function it_should_guarded_the_order_id_field()
+    {
+        $this->assertGuard(['order_id' => 14048343]);
+    }
+    # </editor-fold>
+
+    #-------------------------------------##   <editor-fold desc="The RelationShips">   ##----------------------------------------------------#
+
+    /** @test */
+    public function it_belongs_to_a_order()
+    {
+        $order_id = create('App\Models\Order')->id;
+
+        $receipt = create(Receipt::class, ['order_id' => $order_id]);
+
+        $this->assertEquals($order_id, $receipt->receipt->id);
+    }
+
+    /** @test */
+    public function it_has_many_transactions()
+    {
+        $transactions = create(Transaction::class, [
+            'receipt_id' => $this->receipt->id
+        ], 2);
+
+        $transactions->each(function ($transaction) {
+            $this->assertTrue(
+                $this->receipt->transactions->contains($transaction)
+            );
+        });
+    }
+
+    # </editor-fold>
+
+}
