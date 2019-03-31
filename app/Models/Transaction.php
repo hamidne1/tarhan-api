8ad60da49685f1895a14a6616724f9f6bf8b75d5
hasanteymoori <hasan.teymoori1372@gmail.com>
2019-03-31 12:37:31 +0430

    create order testing and modeling

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,9 +2,51 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+/**
+ * Class Transaction
+ *
+ * @property int $id
+ * @property string status
+ * @property integer amount
+ * @property string ref_id
+ * @property Receipt receipt
+ * @method static | Transaction create($data)
+ */
+class Transaction extends Model {
 
-class Transaction extends Model
-{
-    //
+    /**
+     * {@inheritDoc}
+     */
+    protected $guarded = [
+        'id', 'receipt_id'
+    ];
+
+
+    #-------------------------------------##   <editor-fold desc="The RelationShips">   ##----------------------------------------------------#
+
+    /**
+     * transaction  receipt
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function receipt()
+    {
+        return $this->belongsTo(Receipt::class);
+    }
+
+    # </editor-fold>
+
+
+    /**
+     * checking the transaction state is retry or not
+     *
+     * @return bool
+     */
+    public function isRetry()
+    {
+        return !!in_array($this->status, [
+            \App\Enums\GateWay\TransactionStatusEnum::Success,
+            \App\Enums\GateWay\TransactionStatusEnum::Failed
+        ]);
+    }
 }
---
+++ tests/Unit/Models/TransactionTest.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use App\Enums\GateWay\TransactionStatusEnum;
+use App\Models\Transaction;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TransactionTest extends TestCase {
+    use WithFaker;
+
+    #-------------------------------------##   <editor-fold desc="setUp">   ##----------------------------------------------------#
+
+    /**
+     * @var Transaction $transaction
+     */
+    protected $transaction;
+
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->transaction = create(Transaction::class);
+    }
+
+    /** @test */
+    public function it_should_extends_base_model()
+    {
+        $this->assertTrue(
+            is_subclass_of(
+                $this->transaction, 'App\Models\Model'
+            )
+        );
+    }
+
+    # </editor-fold>
+
+    #-------------------------------------##   <editor-fold desc="The Guarded">   ##----------------------------------------------------#
+
+    /**
+     * checking guard data
+     *
+     * @param array $guardData
+     */
+    protected function assertGuard(array $guardData)
+    {
+        $this->transaction->update(
+            raw(Transaction::class, $guardData)
+        );
+        $this->assertDatabaseMissing('transactions', $guardData);
+    }
+
+    /** @test */
+    public function it_should_guarded_the_id_field()
+    {
+        $this->assertGuard(['id' => 14048343]);
+    }
+
+    /** @test */
+    public function it_should_guarded_the_receipt_id_field()
+    {
+        $this->assertGuard(['receipt_id' => 14048343]);
+    }
+    # </editor-fold>
+
+    #-------------------------------------##   <editor-fold desc="The RelationShips">   ##----------------------------------------------------#
+
+    /** @test */
+    public function it_belongs_to_a_receipt()
+    {
+        $receipt_id = create('App\Models\Receipt')->id;
+
+        $transaction = create(Transaction::class, ['receipt_id' => $receipt_id]);
+
+        $this->assertEquals($receipt_id, $transaction->receipt->id);
+    }
+
+    # </editor-fold>
+
+    #-------------------------------------##   <editor-fold desc="The Method">   ##----------------------------------------------------#
+
+    /** @test */
+    public function it_check_the_transaction_is_retry_by_customer()
+    {
+        $this->assertFalse($this->transaction->isRetry());
+
+        $this->transaction->update([
+            'status' => TransactionStatusEnum::Success
+        ]);
+        $this->assertTrue($this->transaction->isRetry());
+
+        $this->transaction->update([
+            'status' => TransactionStatusEnum::Failed
+        ]);
+        $this->assertTrue($this->transaction->isRetry());
+
+    }
+
+    # </editor-fold>
+
+}
