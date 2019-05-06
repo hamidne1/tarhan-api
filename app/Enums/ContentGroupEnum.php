b541481852ea60fd986abfc2ddba396cd5e7417b
hasanteymoori <hasan.teymoori1372@gmail.com>
2019-05-06 08:19:38 +0430

    add content group for enum of contents group

--- app/Enums/ContentGroupEnum.php
+++ app/Enums/ContentGroupEnum.php
@@ -6,4 +6,11 @@ final class ContentGroupEnum extends Enum {
 
     const Header = 'Header';
     const Slider = 'Slider';
+    const Sidebar1 = 'Sidebar1';
+    const Sidebar2 = 'Sidebar2';
+    const Row1 = 'Row1';
+    const Row2 = 'Row2';
+    const Row3 = 'Row3';
+    const Row4 = 'Row4';
+    const Row5 = 'Row5';
 }
