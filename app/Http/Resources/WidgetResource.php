6a209a3ec98cf47805a08d6a640aa099238c270f
hasanteymoori <hasan.teymoori1372@gmail.com>
2019-05-06 08:29:09 +0430

    update resources

--- app/Http/Resources/WidgetResource.php
+++ app/Http/Resources/WidgetResource.php
@@ -8,7 +8,7 @@ class WidgetResource extends JsonResource {
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request $request
+     * @param \Illuminate\Http\Request $request
      * @return array
      */
     public function toArray($request)
