7879635eb6208bca872803345db9f63666a12a0d
HamidNE <hamidne@mail.ru>
2019-06-30 08:29:18 +0430

    add parent_id to contexts resource

--- app/Http/Resources/ContextResource.php
+++ app/Http/Resources/ContextResource.php
@@ -2,8 +2,19 @@
 
 namespace App\Http\Resources;
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @property integer id
+ * @property integer page_id
+ * @property integer parent_id
+ * @property integer category_id
+ * @property string slug
+ * @property string icon
+ * @property string href
+ * @property string value
+ */
 class ContextResource extends JsonResource
 {
     /**
@@ -14,7 +25,7 @@ class ContextResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  Request  $request
      * @return array
      */
     public function toArray($request)
@@ -22,6 +33,7 @@ class ContextResource extends JsonResource
         return [
             'id' => $this->id,
             'page_id' => $this->page_id,
+            'parent_id' => $this->parent_id,
             'category_id' => $this->category_id,
             'slug' => $this->slug,
             'icon' => $this->icon,
