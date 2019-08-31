260788f9606a5554b5088da0d6391b10477c37aa
HamidNE <hamidne@mail.ru>
2019-08-31 11:14:16 +0430

    add admins database for seed

---
+++ database/seeds/database/admins.sql
@@ -0,0 +1,6 @@
+INSERT INTO admins
+    (id, username, name, password, active, deleted_at)
+VALUES
+    (1, 'hamid', 'حمید نصر', '$2y$10$Ae1qyE65oaeT6THFLHcNROPEMhAz6mnVhaJYmSPkKdq28de3tBHsG', 1, NULL),
+    (2, 'alisha', 'علی شمسی', '$2y$10$0Ulk7eIjTvOQLjwBbxWp3Ozc.bYaEih7IkjcYUI/Kwi.LZvOAK.GS', 1, NULL),
+    (3, 'mostafa', 'مصطفی غلامی', '$2y$10$qabwGDBXpfiZdsdc8msDg.q1kl/5mlCyvDJ35EwH90miI1EEkf7km', 1, NULL);
